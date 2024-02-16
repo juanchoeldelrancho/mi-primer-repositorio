a23c1e8092b55174ff704c6380f2fef5ce6af3aa
juanchoeldelrancho <iju4nch0@gmail.com>
2024-02-15 19:45:46 -0600

    linea de tiempo divergente!

--- ejemplo1git.c
+++ ejemplo1git.c
@@ -1,1 +1,12 @@
-hola mundo 
+#include <stdio.h>
+
+
+int main(){
+	printf("esta es una nueva rama!\n\n");
+	printf("creada usando checkout y modificando este archivo\n!");
+	printf("para despues usar git add y git commit -m\n");
+	printf("de esta forma, creamos una linea del tiempo paralela!!!\n");
+	
+	
+return 0;
+}
